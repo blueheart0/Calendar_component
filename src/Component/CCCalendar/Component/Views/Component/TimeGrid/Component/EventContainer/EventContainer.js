c2ba47bba051274568bde7f31451bb72745ff345
SeungHyun Kim <k10130219@gmail.com>
2020-02-21 10:07:35 +0900

    CSS Edit

--- src/Component/CCCalendar/Component/Views/Component/TimeGrid/Component/EventContainer/EventContainer.js
+++ src/Component/CCCalendar/Component/Views/Component/TimeGrid/Component/EventContainer/EventContainer.js
@@ -13,7 +13,8 @@ const useStyle = makeStyles(
       top: 0,
       left: 0,
       width: "100%",
-      height: "100%"
+      height: "100%",
+      overflow: "hidden"
     },
     is__hover: {
       backgroundColor: "red",
